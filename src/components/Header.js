e236f038d4d0d8ab102d521faf785ad5fbd30e9a
Tejendra Kumar <cstejendra@gmail.com>
2021-06-16 12:11:32 +0530

    Minor Ui Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ handleToggleDarkMode }) => {
   return (
     <div className="header">
-      <h1>Notes</h1>
+      <h1>Notes-App</h1>
       <button
         onClick={() => handleToggleDarkMode((prevDarkMode) => !prevDarkMode)}
         className="save"
